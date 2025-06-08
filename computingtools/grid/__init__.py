3138dca3c866bb79c65da0a8ea0a05106e5d1351
Konstantin Epishin <konstantinepishin@icloud.com>
2025-06-08 18:00:42 +0300

    Update desc for Grid class

---
+++ computingtools/grid/__init__.py
@@ -0,0 +1,5 @@
+__all__ = (
+    "Grid",
+)
+
+from .grid import Grid
--- computingtools/grid/grid.py
+++ computingtools/grid/grid.py
@@ -1,2 +1,7 @@
 class Grid:
+    """
+        This class is an implementation of a smart grid that allows
+        changing the step and topology depending
+        on the current convergence of the solution.
+    """
     pass

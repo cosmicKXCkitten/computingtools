91c5bb6fe3e36b5e48aad31613b43c35edd1c2ff
Konstantin Epishin <konstantinepishin@icloud.com>
2025-06-08 18:00:06 +0300

    Add new class for represent a Task

---
+++ computingtools/task/task.py
@@ -0,0 +1,9 @@
+class Task:
+    """
+        This class represents a problem that needs 
+        to be solved using a numerical method.
+
+        Any numerical method of this library works
+        only with problems presented by this class.
+    """
+    pass

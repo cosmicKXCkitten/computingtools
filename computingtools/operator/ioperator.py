26175730dcc4b73f00345cce103e836a793b5224
Konstantin Epishin <konstantinepishin@icloud.com>
2025-06-08 18:18:52 +0300

    Add interface for difference operator

---
+++ computingtools/operator/ioperator.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class IOperator(ABC):
+    """
+        Interface for difference operator.
+
+        The difference operator is an abstraction from 
+        the theory of numerical methods that allows
+        one to specify a difference scheme for a problem.
+    """
+    pass
